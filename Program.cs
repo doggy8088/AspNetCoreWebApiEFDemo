44f46d9ab093163ac5f8e85c079e220a03a5ba82
Will <doggy.huang@gmail.com>
2023-04-19 07:16:45 +0800

    Add DI for ContosoUniversityContext and ConnectionStrings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using AspNetCoreWebApiEFDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ContosoUniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
